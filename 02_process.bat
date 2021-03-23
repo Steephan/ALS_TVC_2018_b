26745f90e5dafd46f38f2ef6b45e81ffc7825a4a
stlange²git config --global user.email stlange@awi.de² <stephan.lange@awi.de>
2021-03-23 09:00:51 +0100

    update processing

--- 02_process.bat
+++ 02_process.bat
@@ -86,7 +86,7 @@ rem --- 08 DTM generation using OPALS (DTM module) ---
 set COORD=-coord_ref_sys EPSG:32608
 
 rem filter terrain points
-opalsImport -inf 02_intermediate\ALS1_all_echotypes_SOR_terrainprob.txt -outf 02_intermediate\ALS1_all_echotypes_SOR_terrainprob.odm -iformat iformat_terrainProb.xml -tilesize 80.0
+opalsImport -inf 02_intermediate\ALS_all_echotypes_SOR_terrainprob_all.txt -outf 02_intermediate\ALS1_all_echotypes_SOR_terrainprob.odm -iformat iformat_terrainProb.xml -tilesize 80.0
 
 rem RobFilter
 opalsRobFilter -inFile 02_intermediate\ALS1_all_echotypes_SOR_terrainprob.odm -debugOutFile 02_intermediate\ALS1_all_echotypes_SOR_terrainprob_grdPts.xyz -points_in_memory 16000000 -filter "Generic[_TerrainProb>0.0]" -sigmaApriori "_TerrainProb>0.5 ? 0.25 : 0.5"
@@ -142,14 +142,14 @@ opalsStatFilter -inFile 04_rasters\ALS_all_echotypes_SOR_nZ_max_1m.tif -outFile 
 opalsAlgebra -inFile 04_rasters\ALS_all_echotypes_SOR_nZ_max_1m.tif 04_rasters\ALS_all_echotypes_SOR_nZ_max_1m_smooth.tif -outFile 04_rasters\ALS_all_echotypes_SOR_nZ_max_1m_filled.tif -formula "return r[1] if ( r[0] is None) else r[0]" -gridSize 1.0 %COORD% %LIM%
 
 rem rem calculate VegRatio: Number of points with vegetation heights> 1.5 m / number of all points
-opalsCell -inf 02_intermediate\ALS_all_echotypes_SOR_terrainprob.odm -outf 04_rasters\ALS_all_echotypes_SOR_pcount_gt150_1m.tif -cellsize 1.0 -feature pcount -filter "Generic[_nZ_corr>= 1.5]" %COORD% %LIM%
-opalsCell -inf 02_intermediate\ALS_all_echotypes_SOR_terrainprob.odm -outf 04_rasters\ALS_all_echotypes_SOR_pcount_1m.tif -cellsize 1.0 -feature pcount %COORD% %LIM%
+opalsCell -inf 02_intermediate\ALS1_all_echotypes_SOR_terrainprob.odm -outf 04_rasters\ALS_all_echotypes_SOR_pcount_gt150_1m.tif -cellsize 1.0 -feature pcount -filter "Generic[_nZ_corr>= 1.5]" %COORD% %LIM%
+opalsCell -inf 02_intermediate\ALS1_all_echotypes_SOR_terrainprob.odm -outf 04_rasters\ALS_all_echotypes_SOR_pcount_1m.tif -cellsize 1.0 -feature pcount %COORD% %LIM%
 opalsAlgebra -inf 04_rasters\ALS_all_echotypes_SOR_pcount_gt150_1m.tif 04_rasters\ALS_all_echotypes_SOR_pcount_1m.tif -outf 04_rasters\ALS_all_echotypes_SOR_vegratio150cm_1m.tif -formula "float(r[0]) / float(r[1])" -gridsize 1.0 %COORD% %LIM%
 
 rem --- 11 Extraction of individual trees from DSM ---
 
 rem generate DSM and fill
-opalsCell -inf 02_intermediate\ALS_all_echotypes_SOR_terrainprob.odm -outf 04_rasters\ALS_all_echotypes_SOR_Z_max_1m.tif -cellsize 1.0 -feature max %COORD% %LIM%
+opalsCell -inf 02_intermediate\ALS1_all_echotypes_SOR_terrainprob.odm -outf 04_rasters\ALS_all_echotypes_SOR_Z_max_1m.tif -cellsize 1.0 -feature max %COORD% %LIM%
 opalsStatFilter -inFile 04_rasters\ALS_all_echotypes_SOR_Z_max_1m.tif -outFile 04_rasters\ALS_all_echotypes_SOR_Z_max_1m_smooth.tif -feature mean -kernelSize 3 %COORD% %LIM%
 opalsAlgebra -inFile 04_rasters\ALS_all_echotypes_SOR_Z_max_1m.tif 04_rasters\ALS_all_echotypes_SOR_Z_max_1m_smooth.tif -outFile 04_rasters\ALS_all_echotypes_SOR_Z_max_1m_filled.tif -formula "return r[1] if ( r[0] is None) else r[0]" -gridSize 1.0 %COORD% %LIM%
 
